9ad65218288b7d2ad2a28c2f1b445b9104e66a5d
Wisnunu <104215098+Wisnunu@users.noreply.github.com>
2022-04-22 22:37:02 +0700

    Add files via upload

---
+++ UTS_WISNU AGUS SEPTIAN_KECERDASAN BUATAN_191011400313_06TPLM003.py
@@ -0,0 +1,10 @@
+#Deklarasi variabel
+var_nilai = 0
+
+#Perulangan WHILE
+while (var_nilai < 10) :
+     print("Perulangan Ke ",var_nilai," (",var_nilai,"antara 0 sampai 10)Bernilai True")
+     var_nilai +=1
+
+#Diluar Perulangan
+print("var_nilai = ",int(var_nilai)," = 10. Bernilai False")
